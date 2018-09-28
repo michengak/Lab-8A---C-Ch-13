999de664afe23b5e5ef11e3bac3dfa1ff0b609cb
michel ngako <emoumichel@gmail.com>
2018-09-28 08:57:33 -0400

    Add project files.

---
+++ Drawing/DrawingShape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+
+namespace Drawing
+{
+    abstract class DrawingShape
+    {
+        private readonly object shape;
+        protected int size;
+        protected int locX = 0, locY = 0;
+
+        public virtual void Draw(Canvas canvas)
+
+        {
+            if (this.shape == null)
+            {
+                throw new InvalidOperationException("Shape is null");
+            }
+
+            this.shape.Height = this.size;
+            this.shape.Width = this.size;
+            Canvas.SetTop(this.shape, this.locY);
+            Canvas.SetLeft(this.shape, this.locX);
+            canvas.Children.Add(this.shape);
+
+
+        }
+    }
+}
---
+++ Drawing/Square.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+
+namespace Drawing
+{
+    class Square: DrawingShape, IDraw, IColor
+    {
+        public Square(int sideLength)
+            : base(sideLength)
+        {
+
+        }
+        private int sideLength;
+        private int locX = 0, locY = 0;
+        private Rectangle rect = null;
+        private Rectangle shape;
+
+        public Square(int sideLength) => this.sideLength = sideLength;
+
+        public override void Draw(Canvas canvas)
+        {
+            if (this.shape != null)
+            {
+                canvas.Children.Remove(this.shape);
+            }
+            else
+            {
+                this.shape = new Rectangle();
+            }
+            base.Draw(canvas);
+
+            this.rect.Height = this.sideLength;
+            this.rect.Width = this.sideLength;
+            Canvas.SetTop(this.rect, this.locY);
+            Canvas.SetLeft(this.rect, this.locX);
+            canvas.Children.Add(this.rect);
+
+        }  
+
+        
+
+      
+    }
+
+   
+}
